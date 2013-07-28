59837dcd8608c993f67b30d0e299149958a833c2
sws <sws@c81d1ba1-7483-4aa6-992d-90018fdc1311>
2013-07-28 16:27:59 +0000

    * Disable annoying sound spam from droidplant * Make imp arty even louder
    git-svn-id: http://svn.imperialwinter.com/repo/trunk/swiw.sdd@45 c81d1ba1-7483-4aa6-992d-90018fdc1311

--- gamedata/sounds.lua
+++ gamedata/sounds.lua
@@ -1,14 +1,12 @@
 local Sounds = {
 	SoundItems = {
-		default = {
-			gain = 0.5,
-		},
 		Argh_LargeGun = {
-			gain = 2.0,
+			gain = 4.0,
 		},
 		xplolrg4 = {
-			gain = 2.0,
-		}
+			gain = 4.0,
+		},
+		
 	},
 }
 
